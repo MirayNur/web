7646e19db2570c96878195bc9f68d791d1c57f6a
MirayNur <miraynurarslan192@gmail.com>
2024-05-24 11:47:00 +0300

    Add files via upload

---
+++ miray_uygulama/js/deneme.js
@@ -0,0 +1,18 @@
+function resmiGoster1()
+{
+	 document.getElementById("resim1").style.display="block";
+	 document.getElementById("resim2").style.display="none";
+	 document.getElementById("resim3").style.display="none";
+}
+function resmiGoster2()
+{
+	 document.getElementById("resim1").style.display="none";
+	 document.getElementById("resim2").style.display="block";
+	 document.getElementById("resim3").style.display="none";
+}
+function resmiGoster3()
+{
+	 document.getElementById("resim1").style.display="none";
+	 document.getElementById("resim2").style.display="none";
+	 document.getElementById("resim3").style.display="block";
+}
